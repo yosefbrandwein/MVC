f8a76c5a4dbaae2fff962d6b61bcfb5333c79e1e
yosef brandwein <yosefdbrandwein@gmail.com>
2019-08-02 15:02:46 -0400

    Updated ActionResult

---
+++ MVC1/Controllers/ComicsBookController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVC1.Controllers
+{
+    public class ComicsBookController : Controller
+    {
+        public ActionResult Hello()
+        {
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Thursday)
+            {
+                return Redirect("/");
+            }
+            return Content ("Hello World!!!");
+        
+            
+        }
+    }
+}
